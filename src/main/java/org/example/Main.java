202cb8ec5dbbb0b0ae5a11edf092ad8aab6c45bd
Roottoot <syuleiman@gmail.com>
2025-07-01 11:39:29 +0200

    Done

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,25 @@
 package org.example;
 
+import java.util.Arrays;
+
 public class Main {
+    public static String[] COMMONPASSWORDS={"Passwort1","Aa345678"};
     public static void main(String[] args) {
         System.out.println("Initial Commit");
+    }
+    public static boolean checkIfPasswordIsLongerThanEightChar(String password){
+        return password.length()>8;
+    }
+    public static boolean checkIfNumsAreIncluded(String password){
+        return password.matches(".*\\d.*");
+    }
+    public static boolean checkBigAndSmallChar(String password){
+        return password.matches(".*[A-Z].*") && password.matches(".*[a-z].*");
+    }
+    public static boolean commonPassword(String password){
+        return Arrays.asList(COMMONPASSWORDS).contains(password);
+    }
+    public static boolean specialChar(String password){
+        return password.matches(".*[^A-Za-z0-9].*");
     }
 }
---
+++ src/test/java/org/example/MainTest.java
@@ -0,0 +1,46 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+
+    @Test
+    void checkIfPasswordIsLongerThanEightChar() {
+        assertTrue(Main.checkIfPasswordIsLongerThanEightChar("MorethanEightChar123!"));
+    }
+
+    @Test
+    void checkIfNumsAreIncluded() {
+        assertTrue(Main.checkIfNumsAreIncluded("MorethanEightChar123!"));
+    }
+
+    @Test
+    void checkBigAndSmallChar() {
+        assertTrue(Main.checkBigAndSmallChar("MorethanEightChar123!"));
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "Passwort1 , true",
+            "Aa345678 , true",
+            "UniquePass!123 , false"
+    })
+    void commonPassword(String password,boolean check) {
+        assertEquals(Main.commonPassword(password),check);
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "Passwort1! , true",
+            "Aa345678@ , true",
+            "UniquePass123 , false"
+    })
+    void specialChar(String password,boolean check) {
+        assertEquals(Main.specialChar(password),check);
+    }
+}
